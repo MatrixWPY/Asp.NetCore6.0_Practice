34cb29a4a2361357842d6f2743411eae9e53782b
MatrixWPY <matrix.wpy@gmail.com>
2023-02-13 17:38:37 +0800

    [Add]WebChat

---
+++ SignalR/WebChat/WebChat/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebChat.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

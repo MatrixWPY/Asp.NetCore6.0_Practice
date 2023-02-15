38424fd054b16e03805f6519f125d33502e4b9f4
MatrixWPY <matrix.wpy@gmail.com>
2023-02-15 18:43:13 +0800

    [Add]RabbitMQ

---
+++ RabbitMQ/PatternTest/Producer/RabbitMQHelper.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+
+namespace Producer
+{
+    /// <summary>
+    /// RabbitMQ帮助类
+    /// </summary>
+    public class RabbitMQHelper
+    {
+        private static ConnectionFactory factory;
+        private static object lockObj = new object();
+
+        /// <summary>
+        /// 获取单个RabbitMQ连接
+        /// </summary>
+        /// <returns></returns>
+        public static IConnection GetConnection()
+        {
+            if (factory == null)
+            {
+                lock (lockObj)
+                {
+                    if (factory == null)
+                    {
+                        factory = new ConnectionFactory
+                        {
+                            HostName = "localhost", //ip
+                            Port = 5672,            //端口
+                            UserName = "guest",     //账号
+                            Password = "guest",     //密码
+                            VirtualHost = "DevTest" //虚拟主机
+                        };
+                    }
+                }
+            }
+            return factory.CreateConnection();
+        }
+    }
+}

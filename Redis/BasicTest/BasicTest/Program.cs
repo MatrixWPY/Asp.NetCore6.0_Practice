96052026c25cc570c14721111008c4a38212156d
MatrixWPY <matrix.wpy@gmail.com>
2023-02-20 22:03:42 +0800

    [Add]Redis 1.BasicTest

---
+++ Redis/BasicTest/BasicTest/Program.cs
@@ -0,0 +1,43 @@
+﻿using BasicTest;
+using System.Text.Json;
+
+var connection = new RedisConnection();
+var db = connection.Connect("127.0.0.1:6379");
+//db = connection.GetDatabase("127.0.0.1:6379");
+
+var Data = (Member)null;
+if (db.IsExist("Data"))
+{
+    Console.WriteLine("Data exist in redis");
+
+    Data = db.Get<Member>("Data");
+    Console.WriteLine("Get Data from redis");
+}
+else
+{
+    Console.WriteLine("Data not exist in redis");
+
+    var model = new Member()
+    {
+        Name = "Matrix",
+        Level = 1,
+        CreateTime = DateTime.Now
+    };
+
+    db.Set("Data", model, TimeSpan.FromSeconds(30));
+    Console.WriteLine("Set Data to redis");
+
+    Data = db.Get<Member>("Data");
+    Console.WriteLine("Get Data from redis");
+}
+Console.WriteLine(JsonSerializer.Serialize(Data));
+Console.ReadKey();
+
+public class Member
+{
+    public string Name { get; set; }
+
+    public int Level { get; set; }
+
+    public DateTime CreateTime { get; set; }
+}
---
+++ Redis/BasicTest/BasicTest/RedisHelper.cs
@@ -0,0 +1,65 @@
+﻿using StackExchange.Redis;
+using System.Collections.Concurrent;
+using System.Text.Json;
+
+namespace BasicTest
+{
+    public class RedisConnection
+    {
+        private static ConcurrentDictionary<string, Lazy<ConnectionMultiplexer>> s_connectionPool = new();
+
+        public IDatabase Connect(string setting = "localhost")
+        {
+            var connMultiplexer = s_connectionPool.GetOrAdd(setting, new Lazy<ConnectionMultiplexer>(() => ConnectionMultiplexer.Connect(setting)));
+
+            return connMultiplexer.Value.GetDatabase();
+        }
+
+        public IDatabase GetDatabase(string setting = "localhost")
+        {
+            if (s_connectionPool.TryGetValue(setting, out var connMultiplexer))
+            {
+                return connMultiplexer.Value.GetDatabase();
+            }
+
+            return default;
+        }
+    }
+
+    public static class RedisExtension
+    {
+        private static string Serialize<T>(T value, JsonSerializerOptions options)
+        {
+            return JsonSerializer.Serialize(value, options);
+        }
+
+        private static T? Deserialize<T>(RedisValue value, JsonSerializerOptions options)
+        {
+            return JsonSerializer.Deserialize<T>(value, options);
+        }
+
+        public static bool IsExist(this IDatabase db, string key)
+        {
+            return db.KeyExists(key);
+        }
+
+        public static T Get<T>(this IDatabase db, string key, JsonSerializerOptions options = default)
+        {
+            if (db.IsExist(key))
+            {
+                return Deserialize<T>(db.StringGet(key), options);
+            }
+
+            return default;
+        }
+
+        public static void Set<T>(this IDatabase db, string key, T value,
+            TimeSpan? expiry = default,
+            When when = When.Always,
+            CommandFlags flags = CommandFlags.None,
+            JsonSerializerOptions options = default)
+        {
+            db.StringSet(key, Serialize(value, options), expiry, when, flags);
+        }
+    }
+}

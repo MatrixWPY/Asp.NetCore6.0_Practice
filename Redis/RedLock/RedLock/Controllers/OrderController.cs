82e2db63e2de559cb7858cc878c96121acd80cd9
MatrixWPY <matrix.wpy@gmail.com>
2023-04-14 17:23:19 +0800

    [Add]Redis 1.RedLock

---
+++ Redis/RedLock/RedLock/Controllers/OrderController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RedLock.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly ILogger<OrderController> _logger;
+
+        public OrderController(ILogger<OrderController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            _logger.LogInformation($"{Thread.CurrentThread.ManagedThreadId}: request.");
+            string orderNo = null;
+
+            var resource = $"OrderNo:{DateTime.Now:yyyyMMddHHmmssfff}"; //resource lock key
+            var expiry = TimeSpan.FromSeconds(60);                      //lock key expire 時間
+            do
+            {
+                // blocks 直到取得 lock 資源
+                using (var redLock = RedisConnectionFactory.RedisLockFactory.CreateLockAsync(resource, expiry).Result)
+                {
+                    // 確定取得 lock 所有權
+                    if (redLock.IsAcquired)
+                    {
+                        _logger.LogInformation($"{Thread.CurrentThread.ManagedThreadId}: lock start.");
+
+                        // 執行需要獨佔資源的核心工作
+                        orderNo = resource;
+
+                        _logger.LogInformation($"{Thread.CurrentThread.ManagedThreadId}: lock end.");
+                    }
+                    else
+                    {
+                        _logger.LogInformation($"{Thread.CurrentThread.ManagedThreadId}: not get the locker.");
+
+                        SpinWait.SpinUntil(() => false, TimeSpan.FromMilliseconds(1));
+                        resource = $"OrderNo:{DateTime.Now:yyyyMMddHHmmssfff}";
+                    }
+                }
+            } while (orderNo == null);
+
+            _logger.LogInformation($"{Thread.CurrentThread.ManagedThreadId}: response.");
+            return orderNo;
+        }
+    }
+}
---
+++ Redis/RedLock/RedLock/RedisConnectionFactory.cs
@@ -0,0 +1,30 @@
+﻿using RedLockNet.SERedis;
+using RedLockNet.SERedis.Configuration;
+using StackExchange.Redis;
+
+namespace RedLock
+{
+    public class RedisConnectionFactory
+    {
+        private static readonly Lazy<ConnectionMultiplexer> Connection;
+        private static readonly RedLockFactory _redlockFactory;
+
+        static RedisConnectionFactory()
+        {
+            var connectionString = "127.0.0.1:6379";
+            var options = ConfigurationOptions.Parse(connectionString);
+            Connection = new Lazy<ConnectionMultiplexer>(() => ConnectionMultiplexer.Connect(options));
+        }
+
+        public static ConnectionMultiplexer GetConnection() => Connection.Value;
+
+        public static RedLockFactory RedisLockFactory
+        {
+            get
+            {
+                var multiplexers = new List<RedLockMultiplexer> { RedisConnectionFactory.GetConnection() };
+                return RedLockFactory.Create(multiplexers);
+            }
+        }
+    }
+}

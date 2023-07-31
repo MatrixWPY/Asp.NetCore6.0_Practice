0c8880a5497e7d46380e13b53392eb3187df84fd
MatrixWPY <matrix.wpy@gmail.com>
2023-07-31 18:26:57 +0800

    [Add] SSO
    1.Google SSO

---
+++ SSO/TestProjectSSO/TestProjectSSO/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestProjectSSO.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

e64ccab475986cb84befc52ce047f2b30e7be4e7
MatrixWPY <matrix.wpy@gmail.com>
2024-01-15 02:15:24 +0800

    [Edit] Redis
    1.StreamQueue : 新增多個監聽式Consumer

---
+++ Redis/Queue/StreamQueue/Consumer_2/Program.cs
@@ -0,0 +1,46 @@
+﻿using StackExchange.Redis;
+using static Publish.RedisHelper;
+
+RedisConnection.Init("127.0.0.1:6379");
+var redis = RedisConnection.Instance.ConnectionMultiplexer;
+var db = redis.GetDatabase();
+
+string streamKey = "StreamQueue";
+string groupName = "QueueGroup";
+string consumerName = "GroupConsumer_2";
+
+var hasKey = db.KeyExists(streamKey, CommandFlags.None);
+if (hasKey == false)
+{
+    Console.WriteLine($"Didn't found Stream Key.");
+    Console.ReadKey();
+    return;
+}
+var group = db.StreamGroupInfo(streamKey);
+if (group == null || group.Any(e => e.Name.Equals(groupName, StringComparison.OrdinalIgnoreCase)) == false)
+{
+    Console.WriteLine($"Didn't found Group.");
+    Console.ReadKey();
+    return;
+}
+
+Console.WriteLine("Consume from StreamQueue:");
+while (true)
+{
+    var data = db.StreamReadGroup(streamKey, groupName, consumerName, ">", count: 5, noAck: false);
+    if (data.Any())
+    {
+        foreach (var item in data)
+        {
+            item.Values.ToList().ForEach(c =>
+            {
+                Console.WriteLine($"message-id:{item.Id} , {c.Name}:{c.Value}");
+            });
+
+            db.StreamAcknowledge(streamKey, groupName, item.Id);
+            db.StreamDelete(streamKey, new[] { item.Id });
+        }
+
+        Console.WriteLine();
+    }
+}

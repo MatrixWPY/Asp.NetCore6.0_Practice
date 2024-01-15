dbb6f65da57c2cb1b07d184432a78af3a06d94c3
MatrixWPY <matrix.wpy@gmail.com>
2024-01-16 02:16:56 +0800

    [Edit] Redis
    1.StreamQueue : 新增流程說明

--- Redis/Queue/StreamQueue/Consumer_2/Program.cs
+++ Redis/Queue/StreamQueue/Consumer_2/Program.cs
@@ -5,29 +5,32 @@ RedisConnection.Init("127.0.0.1:6379");
 var redis = RedisConnection.Instance.ConnectionMultiplexer;
 var db = redis.GetDatabase();
 
-string streamKey = "StreamQueue";
+string queueName = "StreamQueue";
 string groupName = "QueueGroup";
 string consumerName = "GroupConsumer_2";
 
-var hasKey = db.KeyExists(streamKey, CommandFlags.None);
+#region 前置判斷
+var hasKey = db.KeyExists(queueName, CommandFlags.None);
 if (hasKey == false)
 {
     Console.WriteLine($"Didn't found Stream Key.");
     Console.ReadKey();
     return;
 }
-var group = db.StreamGroupInfo(streamKey);
+
+var group = db.StreamGroupInfo(queueName);
 if (group == null || group.Any(e => e.Name.Equals(groupName, StringComparison.OrdinalIgnoreCase)) == false)
 {
     Console.WriteLine($"Didn't found Group.");
     Console.ReadKey();
     return;
 }
+#endregion
 
 Console.WriteLine("Consume from StreamQueue:");
 while (true)
 {
-    var data = db.StreamReadGroup(streamKey, groupName, consumerName, ">", count: 5, noAck: false);
+    var data = db.StreamReadGroup(queueName, groupName, consumerName, ">", count: 5, noAck: false);
     if (data.Any())
     {
         foreach (var item in data)
@@ -37,8 +40,8 @@ while (true)
                 Console.WriteLine($"message-id:{item.Id} , {c.Name}:{c.Value}");
             });
 
-            db.StreamAcknowledge(streamKey, groupName, item.Id);
-            db.StreamDelete(streamKey, new[] { item.Id });
+            db.StreamAcknowledge(queueName, groupName, item.Id);
+            db.StreamDelete(queueName, new[] { item.Id });
         }
 
         Console.WriteLine();
--- Redis/Queue/StreamQueue/Producer/Program.cs
+++ Redis/Queue/StreamQueue/Producer/Program.cs
@@ -5,20 +5,29 @@ RedisConnection.Init("127.0.0.1:6379");
 var redis = RedisConnection.Instance.ConnectionMultiplexer;
 var db = redis.GetDatabase();
 
-string streamKey = "StreamQueue";
+string queueName = "StreamQueue";
 string groupName = "QueueGroup";
 
-var hasKey = db.KeyExists(streamKey, CommandFlags.None);
+#region 前置設定 Consumer Group
+// 因需先 StreamCreateConsumerGroup 後再 StreamAdd，後續 StreamReadGroup 才讀的到資料
+// 故在 Producer 時先行設置 Consumer 會使用的 GroupName
+
+var hasKey = db.KeyExists(queueName, CommandFlags.None);
 if (hasKey == false)
 {
-    db.StreamAdd(streamKey, "-1", "QueueRoot");
+    // 此時尚未執行 StreamCreateConsumerGroup，故此筆資料不會被 StreamReadGroup 讀出
+    // 此處執行 StreamAdd 目的為使之後的 StreamGroupInfo 可作用
+    db.StreamAdd(queueName, "-1", "QueueRoot");
 }
-var groups = db.StreamGroupInfo(streamKey, CommandFlags.None);
+
+var groups = db.StreamGroupInfo(queueName, CommandFlags.None);
 if (groups == null || groups?.Any(x => x.Name.Equals(groupName, StringComparison.OrdinalIgnoreCase)) == false)
 {
     try
     {
-        db.StreamCreateConsumerGroup(streamKey, groupName, StreamPosition.NewMessages);
+        // 設置 Consumer 會使用的 GroupName
+        // 之後執行 StreamAdd 的資料已可被 StreamReadGroup 讀出
+        db.StreamCreateConsumerGroup(queueName, groupName, StreamPosition.NewMessages);
     }
     catch
     {
@@ -27,12 +36,13 @@ if (groups == null || groups?.Any(x => x.Name.Equals(groupName, StringComparison
         throw;
     }
 }
+#endregion
 
 Console.WriteLine("Produce to StreamQueue:");
 for (int i = 0; i < 10; i++)
 {
     Console.WriteLine($"{i + 1}:Test_{i + 1}");
-    db.StreamAdd(streamKey, $"{i + 1}", $"Test_{i + 1}");
+    db.StreamAdd(queueName, $"{i + 1}", $"Test_{i + 1}");
 }
 
 Console.WriteLine("Produce Finish");

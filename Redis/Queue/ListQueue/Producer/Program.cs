a669131e96943949ee38c94a1638c58f53cfdcd0
MatrixWPY <matrix.wpy@gmail.com>
2024-04-06 17:13:06 +0800

    [Add] Redis
    1.ListQueue

---
+++ Redis/Queue/ListQueue/Producer/Program.cs
@@ -0,0 +1,16 @@
+﻿using static Producer.RedisHelper;
+
+RedisConnection.Init("127.0.0.1:6379");
+var redis = RedisConnection.Instance.ConnectionMultiplexer;
+var db = redis.GetDatabase();
+
+string queueName = "ListQueue";
+
+Console.WriteLine("Produce to ListQueue:");
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"{i + 1}:Test_{i + 1}");
+    db.ListLeftPush(queueName, $"Test_{i + 1}");
+}
+Console.WriteLine("Produce Finish");
+Console.ReadKey();

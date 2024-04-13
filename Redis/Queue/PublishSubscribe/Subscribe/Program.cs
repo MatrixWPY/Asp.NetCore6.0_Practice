78129c62a78d023c78f486d949efaa3389e8c8b8
MatrixWPY <matrix.wpy@gmail.com>
2024-04-13 17:57:48 +0800

    [Edit] Redis
    1.Publish/Subscribe : 訊息調整

--- Redis/Queue/PublishSubscribe/Subscribe/Program.cs
+++ Redis/Queue/PublishSubscribe/Subscribe/Program.cs
@@ -4,11 +4,13 @@ RedisConnection.Init("127.0.0.1:6379");
 var redis = RedisConnection.Instance.ConnectionMultiplexer;
 var db = redis.GetDatabase();
 
+string channel = "topic.test";
+
 var sub = redis.GetSubscriber();
-sub.Subscribe("topic.test", (channel, message) =>
+sub.Subscribe(channel, (chl, msg) =>
 {
-    Console.WriteLine($"{DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss.fff")} - Received message : {message}");
+    Console.WriteLine($"{DateTime.Now : yyyy/MM/dd HH:mm:ss.fff} - Received message : {msg}");
 });
-Console.WriteLine($"{DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss.fff")} - Subscribed channel : topic.test ");
+Console.WriteLine($"{DateTime.Now : yyyy/MM/dd HH:mm:ss.fff} - Subscribed from channel : {channel}");
 
 Console.ReadKey();

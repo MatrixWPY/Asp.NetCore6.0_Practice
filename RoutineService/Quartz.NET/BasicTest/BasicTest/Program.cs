638de84d9edd6793d6b83a1ee1ba042a9d23e05c
MatrixWPY <matrix.wpy@gmail.com>
2025-05-26 00:56:34 +0800

    [Update] Quartz.NET
    1.新增NLog套件
    2.新增Job : WriteLogPerMinuteJob / WriteLogOnTimeJob
    3.Program.cs : 註冊Quartz並設定Job週期

--- RoutineService/Quartz.NET/BasicTest/BasicTest/Program.cs
+++ RoutineService/Quartz.NET/BasicTest/BasicTest/Program.cs
@@ -1,6 +1,49 @@
+﻿using BasicTest.Jobs;
+using NLog.Extensions.Logging;
+using Quartz;
+using Quartz.Simpl;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+#region 註冊NLog
+builder.Logging.AddNLog("nlog.config");
+#endregion
+
+#region 註冊 Quartz 並設定排程
+builder.Services.AddQuartz(q =>
+{
+    q.UseJobFactory<MicrosoftDependencyInjectionJobFactory>();
+
+    var jkPerMinuteJob = new JobKey(nameof(WriteLogPerMinuteJob));
+    q.AddJob<WriteLogPerMinuteJob>(opts =>
+        opts.WithIdentity(jkPerMinuteJob)
+    );
+    q.AddTrigger(opts => opts
+        .ForJob(jkPerMinuteJob)
+        .WithIdentity($"{nameof(WriteLogPerMinuteJob)}-trigger")
+        .WithCronSchedule("0 0/1 * * * ?", x => x.InTimeZone(TimeZoneInfo.Local))
+    );
+
+    var jkOnTimeJob = new JobKey(nameof(WriteLogOnTimeJob));
+    q.AddJob<WriteLogOnTimeJob>(opts =>
+        opts.WithIdentity(jkOnTimeJob)
+    );
+    q.AddTrigger(opts => opts
+        .ForJob(jkOnTimeJob)
+        .WithIdentity($"{nameof(WriteLogOnTimeJob)}-trigger")
+        .WithCronSchedule("0 0 6 * * ?", x => x.InTimeZone(TimeZoneInfo.Local))
+    );
+});
+#endregion
+
+#region 啟動背景 Quartz 服務
+builder.Services.AddQuartzHostedService(opt =>
+{
+    opt.WaitForJobsToComplete = true;
+});
+#endregion
 
 var app = builder.Build();
 

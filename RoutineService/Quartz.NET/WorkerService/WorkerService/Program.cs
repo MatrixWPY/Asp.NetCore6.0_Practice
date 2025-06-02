9e78301314130c441682c3211e011a31972e8caf
MatrixWPY <matrix.wpy@gmail.com>
2025-06-02 17:31:17 +0800

    [Add] WorkerService

---
+++ RoutineService/Quartz.NET/WorkerService/WorkerService/Program.cs
@@ -0,0 +1,20 @@
+﻿using Quartz;
+using Quartz.Simpl;
+
+var builder = Host.CreateDefaultBuilder(args);
+
+builder.ConfigureServices((hostContext, services) =>
+{
+    #region 註冊 Quartz
+    services.AddQuartz(q =>
+    {
+        q.UseJobFactory<MicrosoftDependencyInjectionJobFactory>();
+    });
+    #endregion
+
+    #region 啟用 Quartz HostedService
+    services.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
+    #endregion
+});
+
+builder.Build().Run();
